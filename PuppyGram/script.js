5a1022792397b19f2f87a50f3ec8ad2162ec5879
NicodemusPed <npederson@gicw.org>
2022-06-27 17:39:49 -0700

    update

---
+++ PuppyGram/script.js
@@ -0,0 +1,16 @@
+
+function uploadpup(element){
+    console.log("New Pup")
+    element.innerText = "Uploaded! Thank you.";
+}
+
+function removebutton(element){
+    console.log("try to remove button")
+    element.Remove();
+}
+
+
+function changePicture(element){
+    console.log("New Picture",element)
+    element.Remove();
+}
