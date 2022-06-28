0c26dacec791a797346ca220cb7ca5113b0a50dd
NicodemusPed <npederson@gicw.org>
2022-06-28 11:20:22 -0700

    updates after class

--- PuppyGram/script.js
+++ PuppyGram/script.js
@@ -14,3 +14,27 @@ function changePicture(element){
     console.log("New Picture",element)
     element.Remove();
 }
+
+
+function pawsvideo(element){
+    element.pause()
+}
+
+function changeImg(element){
+    console.log("Hello2",element)
+
+}
+
+function cookieMonster(){
+    console.log("cookie monster", element)
+    var element = document.querySelector("hello3")
+    element.remove()
+}
+
+function incrementLike(id){
+    console.log("hello4", id)
+    var element = document.querySelector(id)
+    console.log("Hello5", element)
+    let likeCount = Number(element,innerText)
+    console.log()
+}
