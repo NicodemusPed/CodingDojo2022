ffb0e4b17496c3b4258fabc5074e8e22fc582dfb
NicodemusPed <npederson@gicw.org>
2022-07-04 21:53:05 -0700

    organize

--- FizzBuzz/FizzBuzz.js
+++
@@ -1,6 +0,0 @@
-
-// for(var i=12; i>3; i-=2) {
-//     console.log(i)
-// }
-
-console.log("hello");
--- Likes/Likes1/Likes2/script.js
+++
@@ -1,9 +0,0 @@
-function incrementLike(id){
-    console.log("More Likes", id);
-    var element =  document.querySelector(id);
-    console.log("element looks like this",  element.innerText);
-    let likeCount = Number (element.innerText);
-    console.log("likeCount");
-    likeCount+=1;
-    element.innerText = likeCount;
-}
