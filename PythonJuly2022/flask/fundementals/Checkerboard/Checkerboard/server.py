19c4eab9116eb662d03ec07b8f3521f26b80698e
NicodemusPed <npederson@gicw.org>
2022-07-12 15:50:02 -0700

    more up

--- PythonJuly2022/flask/fundementals/Checkerboard/Checkerboard/server.py
+++ PythonJuly2022/flask/fundementals/Checkerboard/Checkerboard/server.py
@@ -6,8 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def checker_board():
     
-    return render_template("index.html", times=num_of_hellos, list=student_info, height=height)
+    return render_template("index.html")
 
+@app.route('/4')
+def checker_board4():
+        return render_template("index4.html")
+    
+@app.route('/(x)/(y)')
+    def x():
+    def y():
+        return render_template("indexxy.html")
 
 if __name__=="__main__": 
         app.run(debug=True)
